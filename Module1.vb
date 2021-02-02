e4291ffcf056efb2a3442b483ad109aa94c77ae6
callkend <77285430+callkend@users.noreply.github.com>
2021-02-02 08:44:25 -0700

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,56 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim outcome(10) As Integer
+        Dim number As Integer
+        Randomize()
+
+        'Looks for two random numbers between 1 & 6 and than adds them togther, and stores the
+        'outcome in a array 1000 times.
+        For i = 0 To 999
+            number = CInt(6 * Rnd()) + 1 + CInt(6 * Rnd()) + 1
+            Select Case number
+                Case = 2
+                    outcome(0) += 1
+                Case = 3
+                    outcome(1) += 1
+                Case = 4
+                    outcome(2) += 1
+                Case = 5
+                    outcome(3) += 1
+                Case = 6
+                    outcome(4) += 1
+                Case = 7
+                    outcome(5) += 1
+                Case = 8
+                    outcome(6) += 1
+                Case = 9
+                    outcome(7) += 1
+                Case = 10
+                    outcome(8) += 1
+                Case = 11
+                    outcome(9) += 1
+                Case = 12
+                    outcome(10) += 1
+            End Select
+        Next
+
+        'Formats table and pulls info from array.
+        Console.WriteLine("Roll of the Dice" + vbNewLine)
+        For j = 0 To 20
+            If j > 10 Then
+                Console.Write(vbTab + "|" + CStr(outcome(j - 10)))
+            ElseIf j = 10 Then
+                Console.Write(vbTab + "|" + CStr(j + 2) + vbNewLine)
+                Console.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ " _
+                    + "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _" + vbNewLine)
+                Console.Write(vbTab + "|" + CStr(outcome(j - 10)))
+            Else
+                Console.Write(vbTab + "|" + CStr(j + 2))
+            End If
+        Next
+
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Roll of the Dice")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Roll of the Dice")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e86803f4-f84f-46a8-ab58-f630496dc22a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
